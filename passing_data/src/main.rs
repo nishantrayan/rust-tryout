b7132e873384094171f6c40c0083ff46c696d22d
Nishant Rayan <nishantrayan@gmail.com>
2025-11-06 15:59:29 -0800

    experiment with data passing

---
+++ passing_data/src/main.rs
@@ -0,0 +1,43 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+        // let msg = String::from("hi");
+        // tx.send(msg).unwrap();
+        // println!("Message sent {}", msg);
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+        // let msg = String::from("hi");
+        // tx.send(msg).unwrap();
+        // println!("Message sent {}", msg);
+    });
+    for received in rx {
+        println!("Received: {received}");
+    }
+}
