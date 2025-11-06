e58a0b67314d26631030fcea3b2a440a3cb624d0
Nishant Rayan <nishantrayan@gmail.com>
2025-11-05 19:53:39 -0800

    experiment with drop reference

--- deref_and_drop/src/main.rs
+++ deref_and_drop/src/main.rs
@@ -5,7 +5,6 @@ impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
         MyBox(x)
     }
-
 }
 
 impl<T> Deref for MyBox<T> {
@@ -13,6 +12,16 @@ impl<T> Deref for MyBox<T> {
 
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
 }
 fn main() {
@@ -24,6 +33,16 @@ fn main() {
 
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointers created.");
+    drop(c);
 }
 
 fn hello(name: &str) {
