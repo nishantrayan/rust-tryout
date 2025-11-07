1d7298608ad9799ecc4ba3f55e97a043f76e18d2
Nishant Rayan <nishantrayan@gmail.com>
2025-11-06 16:18:17 -0800

    experiment with shared state using Arc pointers

---
+++ shared_state/src/main.rs
@@ -0,0 +1,26 @@
+use std::sync::Mutex;
+use std::thread;
+use std::sync::Arc;
+fn main() {
+    let m = Mutex::new(0);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }  
+    println!("m = {:?}", m);
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    println!("counter = {:?}", counter.lock().unwrap());
+}
